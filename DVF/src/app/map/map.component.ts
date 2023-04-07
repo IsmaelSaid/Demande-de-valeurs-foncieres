9d7d4d5e24073e891adac4c5081113cb3caec475
Ismael <ismael.saidpro@protonmail.com>
2023-04-07 10:53:03 +0400

    Modif et nettoyage du code Suppression des logs

--- DVF/src/app/map/map.component.ts
+++ DVF/src/app/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit, OnInit } from '@angular/core';
+import { Component, AfterViewInit, OnInit, isDevMode } from '@angular/core';
 import { Carte } from '../models/carte.model';
 import { HttpClientODS } from 'src/services/http-client-open-data-soft.service';
 import { PgsqlBack } from 'src/services/pgsql-back.service';
@@ -9,24 +9,14 @@ import { PgsqlBack } from 'src/services/pgsql-back.service';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements AfterViewInit, OnInit {
-  carte!: Carte
+  carte!: Carte;
 
   constructor(private http: HttpClientODS,private pgsql : PgsqlBack) {
     this.carte = new Carte(http);
-    this.pgsql.get().subscribe((val)=>{
-      console.log(val);
-      
-    })
-    
   }
 
   ngAfterViewInit(): void {
     this.carte.initMap();
-    this.carte.subscribe((value)=>{
-      console.log("ok");
-      
-    })
-
   }
   ngOnInit(): void {
   }
