7db3a3ab0ab54ab8dbf23d85f2b9904666f70d57
Ismael <ismael.saidpro@protonmail.com>
2023-04-08 13:00:06 +0400

    Ajout de test unit

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,18 +1,26 @@
 import unittest
 import requests
 
+"""
+TODO: Il faudrait tester aussi le contenu de la requpete
+"""
+
+
 class TestAPI(unittest.TestCase):
-    def __init__(self):
+    def setUp(self):
         self.api_mutations = "https://dvfapp.fly.dev/api/count/mutations/97418"
-        self.api_typelocal = "https://dvfapp.fly.dev/api/count/typelocal/97418"
+        self.api_type_local = "https://dvfapp.fly.dev/api/count/typelocal/97418"
 
     def test_fonctionnement_API_mutations(self):
         response = requests.get(self.api_mutations)
-        self.assertEquals(response.status_code, 200)
-    
+        self.assertEqual(response.status_code, 200,
+                         "Test de bon fonctionnementapi/count/mutations/:code_insee")
+
     def test_fonctionnement_API_type_local(self):
         response = requests.get(self.api_type_local)
-        self.assertEquals(response.status_code, 200)
+        self.assertEqual(response.status_code, 200,
+                         "Test de bon fonctionnement api/count/typelocal/:code_insee")
+
 
 if __name__ == '__main__':
     unittest.main()
