7541c019ecb74ad8c4dcc92237faa59b45af93d4
SAID Ismael <ismael.saidpro@protonmail.com>
2023-05-01 20:37:24 +0200

    Ajout, fenêtre de visualisation des infographies et ajout d'interactions pour fermer la fenêtre

--- DVF/src/app/analyse-multiple/analyse-multiple.component.ts
+++ DVF/src/app/analyse-multiple/analyse-multiple.component.ts
@@ -4,7 +4,7 @@ import { Analyse } from '../models/analyse.model';
 @Component({
   selector: 'app-analyse-multiple',
   templateUrl: './analyse-multiple.component.html',
-  styleUrls: ['./analyse-multiple.component.css']
+  styleUrls: ['./analyse-multiple.component.css']  
 })
 export class AnalyseMultipleComponent{
   @Input() analyses: Analyse[] = [];
--- DVF/src/app/app.module.ts
+++ DVF/src/app/app.module.ts
@@ -6,25 +6,23 @@ import { AppComponent } from './app.component';
 import { OsmMapComponent } from './osm-map/osm-map.component';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { NgxSidebarControlModule } from '@runette/ngx-leaflet-sidebar';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CanvasModule } from './canvas/canvas.module';
 import { AnalyseMultipleComponent } from './analyse-multiple/analyse-multiple.component';
 import { AnalyseComponent } from './analyse/analyse.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
 
 @NgModule({
   declarations: [
     AppComponent,
-    OsmMapComponent,
-    AnalyseMultipleComponent,
-    AnalyseComponent
+    OsmMapComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     LeafletModule,
     NgxSidebarControlModule,
-    NgbModule
-    
+    NgbModule,
+    CanvasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
